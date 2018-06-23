1eaba7199c8a4b0e8e5190ad2365cfe14baf1f86
Erwan Beauvois <erwan@allmyt.eu>
2018-06-23 17:36:51 +0200

    Implement some presale tests

--- test/GalionToken.test.js
+++ test/GalionToken.test.js
@@ -1,7 +1,8 @@
 const GalionTokenContract = artifacts.require('./GalionToken.sol');
 const ETH = 1e+18;
+const GLN = 1e+18;
 
-contract('GalionTokenContract', function ([owner]) {
+contract('GalionTokenContract', function ([owner, contributor1]) {
   let contract;
 
   beforeEach('setup contract for each test', async function () {
@@ -18,10 +19,76 @@ contract('GalionTokenContract', function ([owner]) {
   });
 
   describe('Presale', async function() {
-    it.skip('should allow to add people to the whitelist');
-    it.skip('should not accept funds from people not whitelisted');
-    it.skip('should not accept funds from people whitelisted for mainsale but not presale');
-    it.skip('should accept funds from people whitelisted for presale');
+    it('should allow to add people to the whitelist', async function() {
+      await contract.addWhitelistedAddressPresaleList([contributor1]);
+      let whitelisted = await contract.checkWhitelistedForPresale(contributor1);
+      assert.equal(whitelisted, true);
+    });
+
+    it('should not accept funds from people not whitelisted', async function() {
+      try {
+        await contract.sendTransaction({ value: 1e+18, from: contributor1 });
+        assert.fail();
+      } catch (error) {
+        assert(error.toString().includes('revert'), error.toString());
+      }
+    });
+
+    it('should not allow to start presale if buy price isn\'t set', async function() {
+      await contract.addWhitelistedAddressPresaleList([contributor1]);
+
+      try {
+        await contract.startPreSale();
+        assert.fail();
+      } catch (error) {
+        assert(error.toString().includes('revert'), error.toString());
+      }
+    });
+
+    it('should allow to start presale if buy price is set', async function() {
+      await contract.addWhitelistedAddressPresaleList([contributor1]);
+      await contract.setBuyPrice(5000);
+      await contract.startPreSale();
+    });
+
+    it('should not accept funds from people whitelisted for presale, before presale starts', async function() {
+      await contract.addWhitelistedAddressPresaleList([contributor1]);
+      await contract.setBuyPrice(5000);
+
+      try {
+        await contract.sendTransaction({ value: 1 * ETH, from: contributor1 });
+        assert.fail();
+      } catch (error) {
+        assert(error.toString().includes('revert'), error.toString());
+      }
+    });
+
+    it('should accept funds from people whitelisted for presale, and give them tokens, if presale is started', async function() {
+      await contract.addWhitelistedAddressPresaleList([contributor1]);
+      await contract.setBuyPrice(5000);
+      await contract.startPreSale();
+      await contract.sendTransaction({ value: 1 * ETH, from: contributor1 });
+
+      const address = await contract.address
+      assert.equal(web3.eth.getBalance(address).toNumber(), 1 * ETH)
+
+      const contributorBalance = await contract.balanceOf(contributor1);
+      assert.equal(contributorBalance.toNumber(), 6000 * GLN)
+    });
+
+
+    it('should not accept funds from people whitelisted for mainsale but not presale', async function() {
+      await contract.addAddressesToMainsaleWhitelist([contributor1]);
+      await contract.setBuyPrice(5000);
+      await contract.startPreSale();
+
+      try {
+        await contract.sendTransaction({ value: 1 * ETH, from: contributor1 });
+        assert.fail();
+      } catch (error) {
+        assert(error.toString().includes('revert'), error.toString());
+      }
+    });
   });
 
   describe('Mainsale', async function() {
