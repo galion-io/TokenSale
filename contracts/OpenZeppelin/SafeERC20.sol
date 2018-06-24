518bab56c817712ea4631b972ee72659bbf73dfc
Emilien <emilien@galion.io>
2018-06-24 19:15:22 +0200

    fix indentation : 4 spaces

--- contracts/OpenZeppelin/SafeERC20.sol
+++ contracts/OpenZeppelin/SafeERC20.sol
@@ -11,22 +11,22 @@ import "./ERC20.sol";
  * which allows you to call the safe operations as `token.safeTransfer(...)`, etc.
  */
 library SafeERC20 {
-  function safeTransfer(ERC20Basic token, address to, uint256 value) internal {
-    require(token.transfer(to, value));
-  }
+    function safeTransfer(ERC20Basic token, address to, uint256 value) internal {
+        require(token.transfer(to, value));
+    }
 
-  function safeTransferFrom(
-    ERC20 token,
-    address from,
-    address to,
-    uint256 value
+    function safeTransferFrom(
+        ERC20 token,
+        address from,
+        address to,
+        uint256 value
   )
     internal
-  {
-    require(token.transferFrom(from, to, value));
-  }
+    {
+        require(token.transferFrom(from, to, value));
+    }
 
-  function safeApprove(ERC20 token, address spender, uint256 value) internal {
-    require(token.approve(spender, value));
-  }
+    function safeApprove(ERC20 token, address spender, uint256 value) internal {
+        require(token.approve(spender, value));
+    }
 }
